629150e513809326c2cd792ab1874476f41eab18
ManiDharun510 <130201030+ManiDharun510@users.noreply.github.com>
2023-09-03 20:27:08 +0530

    Delete adddoctor.js

--- adddoctor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const DoctorSchema = new mongoose.Schema({
-    doctorname: String,
-    doctoremail: String,
-    speciality: String
-})
-
-const DoctorModel = mongoose.model('doctor', DoctorSchema);
-module.exports = DoctorModel;
